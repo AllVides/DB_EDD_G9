62d5b38cec5e782369d3ca9b63682516aeb14dbe
Mynor  Saban <myale2000@gmail.com>
2020-12-14 20:05:59 -0600

    actualizacion

---
+++ MainG.py
@@ -0,0 +1,15 @@
+from DataBaseTree_AVL import AVLTree as datos
+
+
+class Main:
+    
+    def __init__(self):
+        self.database= datos
+    
+    def drop(self,name):
+        self.database.Eliminar(name)
+
+    def createDatabase(database: str) -> int:
+        self.database.
+
+    def alterDatabase(databaseOld, databaseNew) -> int:
